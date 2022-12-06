780f8493cb94da3b679c28c7d0006ea8b85a7c6a
SURENRA YADAV <66127386+surendra-yadav@users.noreply.github.com>
2022-12-06 11:13:46 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,14 +6,14 @@ if (!process.env.WORDPRESS_API_URL) {
 }
 
 /** @type {import('next').NextConfig} */
-// module.exports = {
-//   images: {
-//     domains: [
-//       process.env.WORDPRESS_API_URL.match(/(?!(w+)\.)\w*(?:\w+\.)+\w+/)[0], // Valid WP Image domain.
-//       '0.gravatar.com',
-//       '1.gravatar.com',
-//       '2.gravatar.com',
-//       'secure.gravatar.com',
-//     ],
-//   },
-// }
+module.exports = {
+  images: {
+    domains: [
+      process.env.WORDPRESS_API_URL.match(/(?!(w+)\.)\w*(?:\w+\.)+\w+/)[0], // Valid WP Image domain.
+      '0.gravatar.com',
+      '1.gravatar.com',
+      '2.gravatar.com',
+      'secure.gravatar.com',
+    ],
+  },
+}
