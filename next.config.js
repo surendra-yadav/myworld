84c6c4ce65a3d3c0a101fee6ebaff013e4d9b177
SURENRA YADAV <66127386+surendra-yadav@users.noreply.github.com>
2022-12-06 14:23:26 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,20 +1,9 @@
-if (!process.env.WORDPRESS_API_URL) {
-  throw new Error(`
-    Please provide a valid WordPress instance URL.
-    Add to your environment variables WORDPRESS_API_URL.
-  `)
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  /* config options here */
 }
 
-/** @type {import('next').NextConfig} */
-module.exports = {
-  images: {
-    domains: [
-      process.env.WORDPRESS_API_URL.match(/(?!(w+)\.)\w*(?:\w+\.)+\w+/)[0], // Valid WP Image domain.
-      '1.gravatar.com',
-      '2.gravatar.com',
-      'secure.gravatar.com',
-    ],
-  },
-}
-
+module.exports = nextConfig
 
